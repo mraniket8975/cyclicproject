5207dca1a467f5e1ce31a85795b6d49f811eb4f3
mraniket8975 <128958775+mraniket8975@users.noreply.github.com>
2023-03-30 22:24:02 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static(__dirname  + '/public'))
+
+
+
+
+
+app.listen(5000 , () => console.log('server started at 5000'))
